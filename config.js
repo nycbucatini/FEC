f520549559e1ceade2b08748d7643189053b198b
anthony3662 <anthonyye3662@hotmail.com>
2021-07-18 00:55:47 -0400

    think it works now maybe

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 const IMGKEY = 'b1a20bd700badd2';
 const imgbbKey ='80657138e7d82e45089d1e42304f693f';
-const KEY = 0;
-console.log(GITHUB.REACT_APP_API_KEY);
+const KEY = GITHUB.REACT_APP_API_KEY;
 export default KEY;
 
